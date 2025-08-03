e64803a183fba0611aff3889407ba9e535dee182
elsa <elsaemerllahu12@gmail.com>
2025-08-03 15:00:01 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,15 +15,23 @@ const io = socketIO(server, {
   cors: { origin: '*' }
 });
 
+const roomOffers = {}; // store latest offer per room
+
 io.on('connection', socket => {
   console.log('New user connected:', socket.id);
 
   socket.on('join', (roomId) => {
     socket.join(roomId);
     console.log(`Socket ${socket.id} joined room ${roomId}`);
+
+    // if offer already exists, send it to new joiner
+    if (roomOffers[roomId]) {
+      socket.emit('offer', roomOffers[roomId]);
+    }
   });
 
   socket.on('offer', ({ roomId, offer }) => {
+    roomOffers[roomId] = offer;
     socket.to(roomId).emit('offer', offer);
   });
 
@@ -38,8 +46,13 @@ io.on('connection', socket => {
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
   });
+
+  socket.on('answer', ({ roomId, answer }) => {
+  delete roomOffers[roomId]; // clear offer once answered
+  socket.to(roomId).emit('answer', answer);
 });
 
+});
 
 const PORT = process.env.PORT;
 server.listen(PORT, () => {

d9969954dd6fcc9eccc2176f6ecd982f6f024d27
elsa <elsaemerllahu12@gmail.com>
2025-08-03 13:42:13 +0200

    new files

---
+++ server/index.js
@@ -0,0 +1,38 @@
+// /server/index.js
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: { origin: '*' }
+});
+
+io.on('connection', socket => {
+  console.log('New user connected');
+
+  socket.on('offer', data => {
+    socket.broadcast.emit('offer', data);
+  });
+
+  socket.on('answer', data => {
+    socket.broadcast.emit('answer', data);
+  });
+
+  socket.on('candidate', data => {
+    socket.broadcast.emit('candidate', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
